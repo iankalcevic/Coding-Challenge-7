be5a7dd77bf5fdd7f2ead91c44148bd65d722fe4
iankalcevic <iankalcevic@usf.edu>
2025-02-16 12:27:35 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -8,3 +8,14 @@ console.log('Total invoice: $${total.toFixed(2)}'); //Log total invoice amount
 
 calculateInvoice(100, 0.08, 5); //Expected output: $103.00
 calculateInvoice(500, 0.1, 20); //Expected output: $530.00
+
+// Task 2 - Employee Hourly Wage Calculation
+
+const calculateHourlyWage = function(salary, hoursPerWeek) {
+    const hourlyWage = salary / (hoursPerWeek * 52); //Salary by work hours in a year
+
+    console.log('Hourly wage: $${hourlyWage.toFixed(2)}');
+}; //Log hourly wage 
+
+calculateHourlyWage(52000, 40); //Expeceted output: $25.00
+calculateHourlyWage(75000, 35); //Expected output: $41.21
